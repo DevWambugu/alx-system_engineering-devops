770fc082d3be8a563af97dfc9aa08cef8c07ee61
DevWambugu <venusdata100@gmail.com>
2023-11-09 21:42:48 +0000

    number of reddit subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,10 +13,10 @@ def number_of_subscribers(subreddit):
     '''gets the number of subscribers'''
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+        "User-Agent": "Python/requests"
         }
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    if response.status_code in [302, 404]:
         return 0
     else:
         results = response.json().get("data")
