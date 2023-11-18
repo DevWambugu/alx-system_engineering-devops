675f28ce5440c565072eb3b02c87068241d2272b
DevWambugu <venusdata100@gmail.com>
2023-11-18 08:28:50 +0000

    incraese traffic

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+# increase the number of requests served by ngix
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
+
+# install nginx after increasin traffic
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
---
+++ 0x1B-web_stack_debugging_4/README.md
@@ -0,0 +1,2 @@
+0x1B. Web stack debugging #40x1B. Web stack debugging #4
+Debu your apache server to accept multiple requests without failure
