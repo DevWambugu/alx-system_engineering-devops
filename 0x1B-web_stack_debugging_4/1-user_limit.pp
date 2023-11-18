c80814a55b4673d1b40d06b92f9d902776faf8fa
DevWambugu <venusdata100@gmail.com>
2023-11-18 08:33:35 +0000

    increase limits and user

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,12 @@
+#let the holberton user login. No errors should be experienced.
+# Increase the number of hard files that a holberton user can open
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase the number of soft files that a holberton user can open
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
